aad975b7d2f3699c2dc4cfb501102930dc24f561
maylenepoulsen <mayleneski@hotmail.com>
2020-07-20 10:48:57 -0500

    places and dates first draft

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const burger = document.querySelector(".burger").addEventListener("click", () => {
-    //document.querySelector(".sidebar").classList.add("sidebarActive");
-    document.querySelector('.sidebar').style.width = '400px'
-    
+    document.querySelector('.sidebar').style.width = '400px'  
 });
 
 const closeSidebar = document.querySelector('.close').addEventListener('click', () => {
-    //document.querySelector('.sidebar').classList.remove('sidebarActive')
     document.querySelector('.sidebar').style.width = '0px'
 })
