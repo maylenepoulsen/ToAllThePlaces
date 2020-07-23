4f41b9f0970032e019a3cebc0c43736db8d8a991
maylenepoulsen <mayleneski@hotmail.com>
2020-07-23 16:27:53 -0500

    work on modal in JS and HTML/CSS

--- index.js
+++ index.js
@@ -5,3 +5,16 @@ const burger = document.querySelector(".burger").addEventListener("click", () =>
 const closeSidebar = document.querySelector('.close').addEventListener('click', () => {
     document.querySelector('.sidebar').style.width = '0px'
 })
+
+const openModal = document.querySelectorAll('#modal').forEach((img) => {
+    img.addEventListener('click', () => {
+        modal()
+    })
+})
+
+const modal = () => {
+  const body = document.querySelector('body')  
+  const div = document.createElement('div')
+  div.className = 'modal'
+  body.appendChild(div)
+}
