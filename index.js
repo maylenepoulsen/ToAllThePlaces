fc45f6c38dc64d22e716c91d15ca0e23cecc2cd0
maylenepoulsen <mayleneski@hotmail.com>
2020-07-23 16:57:40 -0500

    close modal prototype working

--- index.js
+++ index.js
@@ -13,8 +13,25 @@ const openModal = document.querySelectorAll('#modal').forEach((img) => {
 })
 
 const modal = () => {
-  const body = document.querySelector('body')  
+  const body = document.querySelector('body')
+
   const div = document.createElement('div')
+  const span = document.createElement('span')
+  span.innerHTML = '&times;'
+  span.className = 'close-btn'
+
+  span.addEventListener('click', () => {
+    closeModal()  
+  })
+
+  div.appendChild(span)
+
   div.className = 'modal'
   body.appendChild(div)
+}
+
+const closeModal = () => {
+  const body = document.querySelector('body')
+  const modal = document.querySelector('.modal')
+  body.removeChild(modal)    
 }
