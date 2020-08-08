98a820b1101a0d17326a86e70d1e2fa6a83fde90
maylenepoulsen <mayleneski@hotmail.com>
2020-08-07 20:06:33 -0500

    work on places data

---
+++ modalContents.js
@@ -0,0 +1,14 @@
+const places = [
+    {
+        id: 1,
+        name: "Lilac Place",
+        image: "./images/lilac-place-provo.jpg",
+        date: "5/01 - 6/02"
+    },
+    {
+        id: 2,
+        name: "Monaco Court",
+        image: "./images/Monaco-Ct-provo.JPG",
+        date: "7/02 - 12/03"
+    } 
+]
