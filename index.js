1647c95890b6f7dc6264398fa7959a26e9da4bc9
maylenepoulsen <mayleneski@hotmail.com>
2020-08-20 15:07:46 -0500

    work on styling the modal

--- index.js
+++ index.js
@@ -25,18 +25,39 @@ const modal = (id) => {
   })
  
   const data = places.find(place => place.id === parseInt(id))
-  console.log(data)
+ 
   const dataDiv = document.createElement('div')
   dataDiv.innerHTML = `<figure>
                          <img src=${data.image} class='modal-img'>
                          <figcaption class='modal-name'><h3>${data.name}&nbsp;&nbsp;&nbsp;${data.date}</h3></figcaption>
-                       </figure>`
+                       </figure>
+                       <div class='modal-description'>${data.description}</div>`
 
   div.appendChild(span)
   div.appendChild(dataDiv)
   
   div.className = 'modal'
+
+  const factDiv = document.createElement('div')
+  factDiv.className = 'fact-list'
+  const ul = factList(data.facts)
+  factDiv.innerHTML = `<h3>Fun Facts about this Place:</h3>`
+  factDiv.appendChild(ul)
+
+  dataDiv.appendChild(factDiv)
+
   body.appendChild(div)
+}
+
+const factList = (array) => {
+  const ul = document.createElement('ul')
+  ul.className = 'fact-list-ul'
+  array.forEach((fact) => {
+    const li = document.createElement('li')
+    li.innerHTML = fact
+    ul.appendChild(li)   
+  })
+  return ul
 }
 
 const closeModal = () => {
