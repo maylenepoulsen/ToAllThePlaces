177501f58bb20fd8076d06584287f22a22a84a0d
maylenepoulsen <mayleneski@hotmail.com>
2020-08-21 14:25:43 -0500

    2nd try with the php

---
+++ modalContents.js
@@ -0,0 +1,106 @@
+const places = [
+    {
+        id: 1,
+        name: "Lilac Place",
+        image: "./images/lilac-place-provo.jpg",
+        date: "5/01 - 6/02",
+        description: 'This apartment was about 300 sq. feet in size with 1 bedroom and a tiny bathroom, a small living area and a tiny galley style kitchen.  The Lilac Place apartment was our very first apartment together as a married couple.',
+        facts: ['No A/C but we did have a portable swamp cooler in our bedroom', 'No storage or cupboards so our vacuum was stored in the living room', 'If we wanted to use the oven, we had to get down on the floor and light the pilot light from underneath', 'The people who bought us out of this contract were most excited about free cable--specifically the BET channel']
+    },
+    {
+        id: 2,
+        name: "Monaco Court",
+        image: "./images/Monaco-Ct-provo.JPG",
+        date: "7/02 - 12/03",
+        description: 'This apartment was about 600 sq. feet in size with 1 bedroom and a bathroom, and an open kitchen living room area.',
+        facts: ['fact about this place 1', 'fact about this place 2', 'fact about this place 3', 'really long fact to test out wrapping the content']
+    },
+    {
+        id: 3,
+        name: "Basement in Orem",
+        image: './images/basement.png',
+        date: "1/04 - 6/04",
+        description: 'This apartment was about 300 sq. feet in size with 1 bedroom and a tiny bathroom, a small living area and a tiny galley style kitchen.',
+        facts: ['fact about this place 1', 'fact about this place 2', 'fact about this place 3', 'really long fact to test out wrapping the content']
+    },
+    {
+        id: 4,
+        name: "West 9th Street",
+        image: "./images/w-9th-st.png",
+        date: "7/04 - 4/05",
+        description: 'This apartment was about 300 sq. feet in size with 1 bedroom and a tiny bathroom, a small living area and a tiny galley style kitchen.',
+        facts: ['fact about this place 1', 'fact about this place 2', 'fact about this place 3', 'really long fact to test out wrapping the content']
+    },
+    {
+        id: 5,
+        name: "Sun Valley Ranch Apartments",
+        image: "./images/sun-valley-ranch-mesa.jpg",
+        date: "5/05 - 4/06",
+        description: 'This apartment was about 300 sq. feet in size with 1 bedroom and a tiny bathroom, a small living area and a tiny galley style kitchen.',
+        facts: ['fact about this place 1', 'fact about this place 2', 'fact about this place 3', 'really long fact to test out wrapping the content']
+    }, 
+    {
+        id: 6,
+        name: "Tolleson House",
+        image: "./images/Tolleson-house.JPG",
+        date: "5/06 - 5/09",
+        description: 'This apartment was about 300 sq. feet in size with 1 bedroom and a tiny bathroom, a small living area and a tiny galley style kitchen.',
+        facts: ['fact about this place 1', 'fact about this place 2', 'fact about this place 3', 'really long fact to test out wrapping the content']
+    },
+    {
+        id: 7,
+        name: "Hansen House",
+        image: "./images/Hansen-house.JPG",
+        date: "6/09 - 6/11",
+        description: 'This apartment was about 300 sq. feet in size with 1 bedroom and a tiny bathroom, a small living area and a tiny galley style kitchen.',
+        facts: ['fact about this place 1', 'fact about this place 2', 'fact about this place 3', 'really long fact to test out wrapping the content']
+    },
+    {
+        id: 8,
+        name: "Catclaw House",
+        image: "./images/Catclaw.png",
+        date: "7/11 - 6/13",
+        description: 'This apartment was about 300 sq. feet in size with 1 bedroom and a tiny bathroom, a small living area and a tiny galley style kitchen.',
+        facts: ['fact about this place 1', 'fact about this place 2', 'fact about this place 3', 'really long fact to test out wrapping the content']
+    },
+    {
+        id: 9,
+        name: "San Antonio House",
+        image: "./images/encino-ash.JPG",
+        date: "7/13 - 7/15",
+        description: 'This apartment was about 300 sq. feet in size with 1 bedroom and a tiny bathroom, a small living area and a tiny galley style kitchen.',
+        facts: ['fact about this place 1', 'fact about this place 2', 'fact about this place 3', 'really long fact to test out wrapping the content']
+    },
+    {
+        id: 10,
+        name: "San Tan Valley House",
+        image: "./images/san-tan-valley.png",
+        date: "8/15 - 2/16",
+        description: 'This apartment was about 300 sq. feet in size with 1 bedroom and a tiny bathroom, a small living area and a tiny galley style kitchen.',
+        facts: ['fact about this place 1', 'fact about this place 2', 'fact about this place 3', 'really long fact to test out wrapping the content']
+    },
+    {
+        id: 11,
+        name: "Holly Springs Apartments",
+        image: "./images/Villages-Pecan-Grove.jpg",
+        date: "3/16 - 2/17",
+        description: 'This apartment was about 300 sq. feet in size with 1 bedroom and a tiny bathroom, a small living area and a tiny galley style kitchen.',
+        facts: ['fact about this place 1', 'fact about this place 2', 'fact about this place 3', 'really long fact to test out wrapping the content']
+    },
+    {
+        id: 12,
+        name: "Fuquay House",
+        image: "./images/New-Moon-Ct-Fuquay.jpg",
+        date: "3/17 - 5/19",
+        description: 'This apartment was about 300 sq. feet in size with 1 bedroom and a tiny bathroom, a small living area and a tiny galley style kitchen.',
+        facts: ['fact about this place 1', 'fact about this place 2', 'fact about this place 3', 'really long fact to test out wrapping the content']
+    },
+    {
+        id: 13,
+        name: "Anderson Mill House",
+        image: "./images/Lobelia.jpg",
+        date: "6/19 -- ",
+        description: 'This apartment was about 300 sq. feet in size with 1 bedroom and a tiny bathroom, a small living area and a tiny galley style kitchen.',
+        facts: ['fact about this place 1', 'fact about this place 2', 'fact about this place 3', 'really long fact to test out wrapping the content']
+    } 
+]
